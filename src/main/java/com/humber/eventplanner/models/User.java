cc5f22cee00c4c8bc01a0999e9958d6ec8b3413f
liploji <zeldrix.don@gmail.com>
2024-08-01 17:38:50 -0400

    Edited models and web database acces

--- src/main/java/com/humber/eventplanner/models/User.java
+++ src/main/java/com/humber/eventplanner/models/User.java
@@ -16,6 +16,7 @@ public class User {
     private Integer id;
     private String first_name;
     private String last_name;
+    private String password;
     private String email;
     private String role;
 }
